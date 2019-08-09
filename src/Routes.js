fef4c18f28b6cc4649024fbf304dfaab04c6066f
SsangWoo <tkddn204@gmail.com>
2019-08-09 18:43:46 +0900

    add Authentications

--- src/Routes.js
+++ src/Routes.js
@@ -2,6 +2,8 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 
 import AppliedRoute from "./components/AppliedRoute";
+import AuthenticatedRoute from "./components/AuthenticatedRoute";
+import UnauthenticatedRoute from "./components/UnauthenticatedRoute";
 import Home from "./containers/Home";
 import Login from "./containers/Login";
 import Signup from "./containers/Signup";
@@ -12,20 +14,31 @@ import NotFound from "./containers/NotFound";
 export default ({ childProps }) => (
   <Switch>
     <AppliedRoute path="/" exact component={Home} props={childProps} />
-    <AppliedRoute path="/login" exact component={Login} props={childProps} />
-    <AppliedRoute path="/signup" exact component={Signup} props={childProps} />
-    <AppliedRoute
+    <UnauthenticatedRoute
+      path="/login"
+      exact
+      component={Login}
+      props={childProps}
+    />
+    <UnauthenticatedRoute
+      path="/signup"
+      exact
+      component={Signup}
+      props={childProps}
+    />
+    <AuthenticatedRoute
       path="/notes/new"
       exact
       component={NewNote}
       props={childProps}
     />
-    <AppliedRoute
+    <AuthenticatedRoute
       path="/notes/:id"
       exact
       component={Notes}
       props={childProps}
     />
+
     {/* Finally, catch all unmatched routes */}
     <Route component={NotFound} />
   </Switch>
