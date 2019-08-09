d047712588fa36ec955b8b56a709ffea2e9e1f72
SsangWoo <tkddn204@gmail.com>
2019-08-09 18:38:18 +0900

    add note apis

--- src/Routes.js
+++ src/Routes.js
@@ -5,6 +5,8 @@ import AppliedRoute from "./components/AppliedRoute";
 import Home from "./containers/Home";
 import Login from "./containers/Login";
 import Signup from "./containers/Signup";
+import NewNote from "./containers/NewNote";
+import Notes from "./containers/Notes";
 import NotFound from "./containers/NotFound";
 
 export default ({ childProps }) => (
@@ -12,6 +14,18 @@ export default ({ childProps }) => (
     <AppliedRoute path="/" exact component={Home} props={childProps} />
     <AppliedRoute path="/login" exact component={Login} props={childProps} />
     <AppliedRoute path="/signup" exact component={Signup} props={childProps} />
+    <AppliedRoute
+      path="/notes/new"
+      exact
+      component={NewNote}
+      props={childProps}
+    />
+    <AppliedRoute
+      path="/notes/:id"
+      exact
+      component={Notes}
+      props={childProps}
+    />
     {/* Finally, catch all unmatched routes */}
     <Route component={NotFound} />
   </Switch>
